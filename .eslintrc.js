fdc7d0290adeb906159b3600a173dd45c21c5d70
Alex Wendland <me@alexwendland.com>
2021-03-09 15:55:13 -0800

    ci: introduce pre-commit to manage prettier, eslint, markdownlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
+  extends: 'react-app',
+  env: {
+    browser: true,
+    es6: true,
   },
-  "plugins": [
-    "react",
-  ],
-  "globals": {
-    "graphql": false,
+  plugins: ['react'],
+  globals: {
+    graphql: false,
+    __PATH_PREFIX__: true,
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-  }
+  },
 }
